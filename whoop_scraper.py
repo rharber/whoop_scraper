9199e9e80d7dfe8fc4db6621e974957054b372f6
rharber <ryanharber@gmail.com>
2022-01-19 16:41:29 -0800

    Refactoring to prep for cycle data. Working with no breaks.

--- whoop_scraper.py
+++ whoop_scraper.py
@@ -45,10 +45,11 @@ class WhoopUser:
         self.password = password
         self.get_token()
         self.start_date = start_date
-        self.set_start_dt()
         self.window_seconds = window_s
         self.interval_seconds = interval_s
-        self.get_data()
+        self.set_api_timestamps()
+        self.get_heartrate_data()
+        # TODO: self.get_cycle_data()
 
     def get_token(self):
         # Post credentials
@@ -71,24 +72,29 @@ class WhoopUser:
         self.userid = r.json()["user"]["id"]
         self.access_token = r.json()["access_token"]
 
-    def get_data(self):
+    def set_api_timestamps(self):
+        self.start_datetime = (
+            datetime.combine(self.start_date, datetime.min.time())
+            if self.start_date
+            else datetime.utcnow()
+        )
         # Compute api start/end timestamps for desired window range
-        api_end_time = self.start_datetime.replace(tzinfo=timezone.utc).strftime(
+        self.api_end_time = self.start_datetime.replace(tzinfo=timezone.utc).strftime(
             "%Y-%m-%dT%H:%M:%S.%fZ"
         )
-        api_start_time = (
+        self.api_start_time = (
             (self.start_datetime - timedelta(seconds=self.window_seconds))
             .replace(tzinfo=timezone.utc)
             .strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         )
 
+    def get_heartrate_data(self):
         # Download heartrate data
         url = "https://api-7.whoop.com/users/{}/metrics/heart_rate".format(self.userid)
 
         params = {
-            "start": api_start_time,
-            "end": api_end_time,
-            # "step": self.interval_seconds,
+            "start": self.api_start_time,
+            "end": self.api_end_time,
             "step": self.interval_seconds,
         }
         # (FORMAT) params = {"start": "2022-01-01T00:00:00.000Z", "end": "2030-01-01T00:00:00.000Z"}
@@ -104,25 +110,47 @@ class WhoopUser:
             exit()
 
         # Convert to JSON
-        self.data_raw = r.json()
+        self.heartrate_data = r.json()
+
+    def get_cycle_data(self):
+        # Download heartrate data
+        url = "https://api-7.whoop.com/users/{}/cycles".format(self.userid)
+
+        params = {
+            "start": self.api_start_time,
+            "end": self.api_end_time,
+        }
+        # (FORMAT) params = {"start": "2022-01-01T00:00:00.000Z", "end": "2030-01-01T00:00:00.000Z"}
+
+        headers = {"Authorization": "bearer {}".format(self.access_token)}
+
+        r = requests.get(url, params=params, headers=headers)
+
+        # Check if user/auth are accepted
+        if r.status_code != 200:
+            msg = "Fail - User ID / auth token rejected."
+            print(f'error msg="{msg}" {time.time_ns()}')
+            exit()
+
+        # Convert to JSON
+        self.sleep_workout_data = r.json()
 
     def print_line_protocol(self):
         try:
-            for heartrate in self.data_raw["values"]:
+            for heartrate in self.heartrate_data["values"]:
                 bpm = heartrate["data"]
                 ns = heartrate["time"] * 1000000
-                # Output line protocol
                 print(f"heartrate,user_id={self.userid} bpm={bpm} {ns}")
+
+            # for day in self.sleep_workout_data:
+            #     #TODO: Implement
+            #     bpm = heartrate["data"]
+            #     ns = heartrate["time"] * 1000000
+            #     print(f"heartrate,user_id={self.userid} bpm={bpm} {ns}")
+
         except Exception as e:
             print(f'error msg="{e}" {time.time_ns()}')
             exit()
-
-    def set_start_dt(self):
-        self.start_datetime = (
-            datetime.combine(self.start_date, datetime.min.time())
-            if self.start_date
-            else datetime.utcnow()
-        )
 
 
 #################################################################
